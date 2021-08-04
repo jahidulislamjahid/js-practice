61bcb7f8590b744d5b10bd9bce36a2457f040f4a
jahid <info.jahidulislamjahid@gmail.com>
2021-08-04 13:03:50 +0600

    continue

--- break-continue.js
+++ break-continue.js
@@ -13,3 +13,20 @@ for(i=0; i<15; i++){
         break;
     }
 }
+
+var num = [20, 50, 89, 65, 32, 73, 55, 121, 98, 322, 88, 99, 95, 222, 300]
+for(i=0; i<num.length; i++){
+    var number = num[i];
+    console.log(number);
+    if(number>80){
+        break;
+    }
+}
+
+for(i=0; i<num.length; i++){ //var 'number' from before.
+    var number = num[i];
+    if(number>80){ //condition for continue. continue skips the parameter that is set
+        continue;
+    }
+    console.log(number);
+}
