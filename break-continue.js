c2a9c92b523d56c4f0f863ea2eab8530b3bda407
jahid <info.jahidulislamjahid@gmail.com>
2021-08-04 12:44:37 +0600

    break added

---
+++ break-continue.js
@@ -0,0 +1,15 @@
+var i = 0;
+while(i<15){
+    console.log(i);
+    if(i==10){ //condition for break
+        break;
+    }
+    i++;
+}
+
+for(i=0; i<15; i++){
+    console.log(i);
+    if(i==8){ //condition for break
+        break;
+    }
+}
