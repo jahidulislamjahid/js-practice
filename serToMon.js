fbd5cd6ddc1e3457a4e8b9f6be96b3545f120db4
jahid <info.jahidulislamjahid@gmail.com>
2021-08-08 23:50:38 +0600

    seertomon

---
+++ serToMon.js
@@ -0,0 +1,12 @@
+function seerToMon(mon){
+    let seerPerMon = 40;
+    if (typeof mon != 'number'){
+    return 'please input a valid quantity'
+    }
+    else{
+        mon = mon / seerPerMon;
+        return mon;
+    }
+}
+let myQuantuty = seerToMon(100);
+console.log(myQuantuty);
