aea1ccddafb860bd68ef5e302c8cc6f5e3775461
jahid <info.jahidulislamjahid@gmail.com>
2021-08-25 17:24:06 +0600

    spread

---
+++ spread.js
@@ -0,0 +1,6 @@
+// getting just the elements from an array 
+//  ... pulls out just the elements from an array.
+const numbers = [12, 50, 80, 56, 23, 45, 59, 78, 20];
+console.log(...numbers);
+// getting the max number from the array
+console.log(Math.max(...numbers));
